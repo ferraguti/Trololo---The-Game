6b5d30af2b681bf761ffe77f46ca3d171e114992
Sergey Mohov <sergey@sergeymohov.com>
2013-11-22 11:43:14 +0100

    Scene changed a bit.

---
+++ Assets/Scripts/Calc.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Calc : MonoBehaviour {
+
+	float output = -29.5f;
+	// Use this for initialization
+	void Start () 
+	{
+		int i = 1;
+		while (output <= 500f)
+		{
+			i++;
+			output += 5f - 0.0909f;
+			Debug.Log(output);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
